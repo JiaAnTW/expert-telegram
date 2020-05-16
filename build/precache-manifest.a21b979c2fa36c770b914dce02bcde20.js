96d1bb124e23132d4811948181cb85a13d30adc2
JiaAnTW <39335864+JiaAnTW@users.noreply.github.com>
2020-05-16 19:42:38 +0800

    try to fix gihub page bug

---
+++ build/precache-manifest.a21b979c2fa36c770b914dce02bcde20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011722e6565b15e6f2f00d2efe2bb81b",
+    "url": "/JiaAnTW/invAccountingWeb/index.html"
+  },
+  {
+    "revision": "6c63d78cc7608e2e92b8",
+    "url": "/JiaAnTW/invAccountingWeb/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b00c1371fd95f511960b",
+    "url": "/JiaAnTW/invAccountingWeb/static/css/main.64619688.chunk.css"
+  },
+  {
+    "revision": "6c63d78cc7608e2e92b8",
+    "url": "/JiaAnTW/invAccountingWeb/static/js/2.3071a0da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/JiaAnTW/invAccountingWeb/static/js/2.3071a0da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00c1371fd95f511960b",
+    "url": "/JiaAnTW/invAccountingWeb/static/js/main.36e68542.chunk.js"
+  },
+  {
+    "revision": "4d361cbefb8b9b1240ae",
+    "url": "/JiaAnTW/invAccountingWeb/static/js/runtime-main.f12e47ae.js"
+  }
+]);
